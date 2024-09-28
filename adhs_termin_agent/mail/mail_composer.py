a9d7f55ff70fa6b18a9873352e9e39e6c658c90f
jhuss <jacques.huss@andrena.de>
2024-09-28 06:48:26 +0200

    compose first draft

---
+++ adhs_termin_agent/mail/mail_composer.py
@@ -0,0 +1,57 @@
+import dotenv
+from openai import OpenAI
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=dotenv.get_key("../../.env", "OPENROUTER_API_KEY"),
+)
+
+def callApi(prompt: str, model: str, system_message: str = ""):
+    messages = []
+    if system_message.strip() != "":
+        messages.append(
+            {
+                "role": "system",
+                "content": system_message
+            }
+        )
+    messages.append(
+        {
+            "role": "user",
+            "content": prompt
+        }
+    )
+    completion = client.chat.completions.create(
+        model=model,
+        messages=messages
+    )
+    try:
+        return (completion.choices[0].message.content)
+    except:
+        print(completion)
+
+
+def first_draft(thema, specialty, name):
+    prompt_template: str = """Schreibe mir eine Email zum zu einer terminanfrage bei einem Facharzt der {}. Mein Name ist {} und ich suche so bald wie möglich einen Termin zu {}. Beginne mit 'Sehr geehrte Damen und Herren' und Ende mit 'Mit freundlichen Grüßen {}'"""
+    system_prompt: str = "Antworte nur mit dem Email Text. Der Text sollte keine optionalen Felder beinhalten"
+    prompt: str = prompt_template.format(specialty,name,thema,name)
+    return callApi(prompt, "openai/gpt-4o-mini", system_prompt)
+
+
+
+
+def get_user_input(prompt):
+    return input(prompt).strip()
+
+
+def is_valid_name(name):
+    return bool(re.match(r'^[A-Za-z\s]+$', name))
+
+
+
+
+if __name__ == "__main__":
+    email = first_draft("ADS Diagnose","Neurologe", "Anna Karenina")
+    print(email)
+
+
