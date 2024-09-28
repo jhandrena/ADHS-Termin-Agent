fc5b4e1a46bc0c644c618e30d24b81067054cfd3
Florian Egner <f.egner@screensauto.com>
2024-09-28 14:50:55 +0200

    feat: Add debugger statement to fetch available doctors

--- doc-bot/components/doctor-appointment/step6-call-doctors.tsx
+++ doc-bot/components/doctor-appointment/step6-call-doctors.tsx
@@ -19,6 +19,7 @@ export function Step6CallDoctors() {
 
   const fetchAvailableDoctors = async () => {
     try {
+      debugger
       const response = await fetch(`http://127.0.0.1:5000/doctors/phone?date=${encodeURIComponent(dateTime.split('T')[0])}&time=${encodeURIComponent(dateTime.split('T')[1])}`);
       if (!response.ok) {
         throw new Error('Failed to fetch available doctors');
