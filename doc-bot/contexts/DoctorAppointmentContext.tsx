253e50c171b65bf84aeb32c85484fe561aa72dd8
Florian Egner <f.egner@screensauto.com>
2024-09-28 19:16:48 +0200

    feat: Add new context for doctor appointment flow

--- doc-bot/contexts/DoctorAppointmentContext.tsx
+++ doc-bot/contexts/DoctorAppointmentContext.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import { Doctor, searchDoctors, triggerDoctorsSearch } from '../utils/doctor-appointment-utils';
 import Cookies from 'js-cookie';
 
+
 interface DoctorAppointmentState {
   step: number;
   location: string;
