4d9fe7ca7bea3955e34b55412206c0652fde86fc
jhuss <jacques.huss@andrena.de>
2024-09-28 06:52:54 +0200

    added doctor filter

--- adhs_termin_agent/mail/doctor_filter.py
+++ adhs_termin_agent/mail/doctor_filter.py
@@ -1,5 +1,3 @@
-
-
 import json
 
 
@@ -8,8 +6,10 @@ def parse_output_json() -> list:
         data = json.load(file)
     return data
 
+
 def filter_doctors_with_email(data: list) -> list:
     return [doctor for doctor in data if doctor['terminOptionen']['email'] is True]
+
 
 def get_user_exclusions(doctors: list) -> list:
     print("Available doctors:")
@@ -22,12 +22,14 @@ def get_user_exclusions(doctors: list) -> list:
         return [doctor for i, doctor in enumerate(doctors) if i not in excluded_indices]
     return doctors
 
+
 def get_filtered_doctors(doctors: list) -> list:
     complete_doctors = [
         doctor for doctor in doctors
         if doctor['name'] != "not set" and doctor['email'] != "not set" and doctor['telefon'] != "not set"
     ]
     return complete_doctors
+
 
 if __name__ == "__main__":
     data = parse_output_json()
@@ -40,4 +42,3 @@ if __name__ == "__main__":
     print("Filtered doctors with complete information:")
     for doctor in complete_doctors:
         print(doctor['name'])
-
